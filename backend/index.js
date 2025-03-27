a307d728b5686bf495a4c0da017f093e387164c3
AVTAR SINGH <130889026+Avtarsingh97@users.noreply.github.com>
2025-03-27 15:58:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -80,7 +80,11 @@ io.on('connection',(socket)=>{
 // Default Route
 app.use("/", (req,res)=>{
     res.status(200).json({message : "Server started"});
-    
+    res.setHeader("Access-Control-Allow-Origin", process.env.BASE_URL)
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "1800");
+res.setHeader("Access-Control-Allow-Headers", "content-type");
+res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
 })
 
 // Start Server
