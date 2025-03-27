bfd0e8a10d3e6d1cafd224c8d9e2b1c364c9207e
AVTAR SINGH <130889026+Avtarsingh97@users.noreply.github.com>
2025-03-27 13:54:15 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,8 +1,8 @@
 import {io} from 'socket.io-client';
 
 const socket = io(import.meta.env.VITE_BASE_URL, {
-    withCredentials: true,
-    transports: ["websocket", "polling"]  // Use both WebSocket & Polling
+    withCredentials: true
+
 });
 
 export default socket;
